6b9c1ca6580ba7d528c257b92c937ab003da192a
Firefox <tshankgg@gmail.com>
2023-10-10 18:05:50 -0500

    finished JS, got css working on time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,37 @@
 $(function () {
- 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  var currentTime = dayjs().hour();
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+
+    $(".saveBtn").click(function(){
+      var timeBlockId = $(this).parent().attr("id");
+      var inputValue = $(this).siblings(".description").val();
   
+      localStorage.setItem(timeBlockId, inputValue);
+    })
+  })
+    
+  var currentTime = dayjs().hour();
+  var timeBlocks = $(".time-block").toArray(); 
+
+  for (var i = 0; i < timeBlocks.length; i++) {
+    var $timeBlock = $(timeBlocks[i]);
+    var timeBlockId = $timeBlock.attr("id");
+    var blockHour = timeBlockId.split("-")[1];
+
+    if (blockHour < currentTime) {
+      $timeBlock.addClass("past");
+    } else if (blockHour === currentTime) {
+      $timeBlock.addClass("present");
+    } else {
+      $timeBlock.addClass("future");
+    }}
+
+
+  for (var i = 0; i <= 24; i++) {
+    var timeBlockId = "hour-" + i;
+    var storedInput = localStorage.getItem(timeBlockId);
+    if (storedInput) {
+      $("#"+timeBlockId).find(".description").val(storedInput);
+    }
+  }
+  
+
   $("#currentDay").text(dayjs().format("dddd, MMMM, D"));
-});
